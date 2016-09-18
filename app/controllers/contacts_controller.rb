1ad83b64912dbd80688c41a291e5e693e4d80172
kenji <tachiken0210@gmail.com>
2016-09-18 03:27:53 +0000

    second commit

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,34 +2,30 @@ class ContactsController < ApplicationController
   
   def new
       if params[:back]
-          @contact=Contact.new(contacts_params)
-      else
-     @contact = Contact.new
-  end
+         @contact=Contact.new(contacts_params)
+         elsif
+         @contact = Contact.new
+      end
   end
   
-
    def create
        @contact=Contact.new(contacts_params)
-       
-    if @contact.save
-       redirect_to root_path, notice: "お問い合わせありがとうございました！"
-       else
-        render action: 'new'
-    end
+       if @contact.save
+           redirect_to root_path, notice: "お問い合わせありがとうございました！"
+           elsif
+           render action: 'new'
+       end
    end
-    
-    def confirm
-        @contact=Contact.new(contacts_params)
-        render :new if @contact.invalid?
-           end
-    
-
    
+  def confirm
+      @contact=Contact.new(contacts_params)
+      render :new if @contact.invalid?
+  end
   
   private
-    def contacts_params
+  def contacts_params
       params.require(:contact).permit(:name,:email,:content)
-    end
   end
-    
+  
+end
+  
