f02310e49b415a48f9b42a6a7f421a90b5d61003
kenji tachibana <kenji_tachibana@MBR14030.local>
2016-10-04 21:22:40 +0900

    dive11_2

--- app/mailers/notice_mailer.rb
+++ app/mailers/notice_mailer.rb
@@ -15,7 +15,7 @@ class NoticeMailer < ApplicationMailer
   def sendmail_contact(contact)
     @contact = contact
 
-    mail to:  "tachiken0210@gmail.com",
+    mail to:  contact.email,
             subject: '【Achieve】問い合わせが完了しました'
 end
 
