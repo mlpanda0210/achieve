592ba06266b4b8aa6290ea8f3a32ef5ad57032a0
tachiken0210 <kenji_tachibana@mbr14030.tbwa.jp>
2016-11-15 21:00:21 +0900

    dive15_totyuu

--- db/migrate1/20161112061724_create_comments.rb
+++ db/migrate1/20161112061724_create_comments.rb
@@ -3,7 +3,6 @@ class CreateComments < ActiveRecord::Migration
     create_table :comments do |t|
       t.references :user, index: true, foreign_key: true
       t.references :blog, index: true, foreign_key: true
-      t.text :content
       t.timestamps null: false
     end
   end
