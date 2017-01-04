4387a5f5b27393eedb3eab74066384b2a6c78b27
tachiken0210 <kenji_tachibana@MBR14030.local>
2017-01-04 16:14:21 +0900

    @notification_countのスペルミス修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -19,6 +19,6 @@ CarrierWave.configure do |config|
       config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/開発環境用のバケット名'
     when 'test'
       config.fog_directory = 'テスト環境用のバケット名'
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/テスト環境用のバケット名
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/テスト環境用のバケット名'
   end
 end
